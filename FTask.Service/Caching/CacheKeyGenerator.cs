372bab15049ea822d583a42f890417e6d7e38827
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-09 02:21:22 +0700

    feat: add cache key gen

---
+++ FTask.Service/Caching/CacheKeyGenerator.cs
@@ -0,0 +1,30 @@
+﻿namespace FTask.Service.Caching
+{
+    public class CacheKeyGenerator
+    {
+        public static string GetKeyById(string objectType, string id)
+        {
+            return $"{objectType.ToLower()}:id:{id}";
+        }
+
+        public static string GetKeyByName(string objectType, string name)
+        {
+            return $"{objectType.ToLower()}:name:{name.ToLower()}";
+        }
+
+        public static string GetKeyByPageAndQuantity(string objectType, int pageNumber, int quantity)
+        {
+            return $"{objectType.ToLower()}:page:{pageNumber}:quantity:{quantity}";
+        }
+
+        public static string GetKeyByOtherId(string objectType, string otherName, string otherId)
+        {
+            return $"{objectType.ToLower()}:{otherName}:id:{otherId}";
+        }
+
+        public static string GetKeyByOtherName(string objectType, string otherName, string otherValue)
+        {
+            return $"{objectType.ToLower()}:{otherName}:name:{otherValue.ToLower()}";
+        }
+    }
+}
