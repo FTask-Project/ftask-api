ac23a99d5abde0129c37a054208a8cc8529b15aa
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-15 12:22:36 +0700

    feat: implement task report

---
+++ FTask.API/Controllers/TaskReportController.cs
@@ -0,0 +1,116 @@
+﻿using AutoMapper;
+using FTask.Service.IService;
+using FTask.Service.ViewModel;
+using FTask.Service.ViewModel.RequestVM.CreateTaskReport;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FTask.API.Controllers
+{
+    [Route("api/task-reports")]
+    [ApiController]
+    public class TaskReportController : ControllerBase
+    {
+        private readonly ITaskReportService _taskReportService;
+        private readonly IMapper _mapper;
+
+        public TaskReportController(ITaskReportService taskReportService, IMapper mapper)
+        {
+            _taskReportService = taskReportService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}", Name = nameof(GetTaskReportById))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TaskReportResponseVM))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+        public async Task<IActionResult> GetTaskReportById(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _taskReportService.GetTaskReportById(id);
+                if (result is null)
+                {
+                    return NotFound("Not found");
+                }
+                return Ok(_mapper.Map<TaskReportResponseVM>(result));
+            }
+            else
+            {
+                return BadRequest(new ServiceResponse
+                {
+                    IsSuccess = false,
+                    Message = "Invalid input"
+                });
+            }
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<TaskReportResponseVM>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ServiceResponse))]
+        public async Task<IActionResult> GetTaskReports([FromQuery] int page,
+            [FromQuery] int quantity,
+            [FromQuery] int? taskActivityId)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _taskReportService.GetTaskReports(page, quantity, taskActivityId);
+                return Ok(_mapper.Map<IEnumerable<TaskReportResponseVM>>(result));
+            }
+            else
+            {
+                return BadRequest(new ServiceResponse
+                {
+                    IsSuccess = false,
+                    Message = "Invalid input"
+                });
+            }
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(TaskReportResponseVM))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ServiceResponse))]
+        public async Task<IActionResult> CreateTaskLecturer([FromForm] TaskReportVM resource)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _taskReportService.CreateNewTaskReport(resource);
+                if (result.IsSuccess)
+                {
+                    int id = Int32.Parse(result.Id!);
+                    var existedTaskReport = await _taskReportService.GetTaskReportById(id);
+                    if (existedTaskReport is not null)
+                    {
+                        return CreatedAtAction(nameof(GetTaskReportById), new
+                        {
+                            id = id,
+                        },
+                        _mapper.Map<TaskReportResponseVM>(existedTaskReport));
+                    }
+                    else
+                    {
+                        return BadRequest(new ServiceResponse
+                        {
+                            IsSuccess = false,
+                            Message = "Failed to create new task report",
+                            Errors = new string[1] {"Created report not found"}
+                        });
+                    }
+                }
+                else
+                {
+                    return BadRequest(result);
+                }
+            }
+            else
+            {
+                return BadRequest(new ServiceResponse
+                {
+                    IsSuccess = false,
+                    Message = "Invalid input"
+                });
+            }
+        }
+    }
+}
--- FTask.Service/IService/ITaskReportService.cs
+++ FTask.Service/IService/ITaskReportService.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using FTask.Service.ViewModel.RequestVM.CreateTaskReport;
+using FTask.Service.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FTask.Repository.Entity;
 
 namespace FTask.Service.IService
 {
     public interface ITaskReportService
     {
+        Task<TaskReport?> GetTaskReportById(int id);
+        Task<IEnumerable<TaskReport>> GetTaskReports(int page, int quantity, int? taskActivityId);
+        Task<ServiceResponse> CreateNewTaskReport(TaskReportVM newEntity);
     }
 }
