bd632c0f2a778ee6b0af8e85d73ba8747ac12605
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-15 12:14:30 +0700

    fix(task activity entity): minor fix

--- FTask.Repository/Entity/TaskReport.cs
+++ FTask.Repository/Entity/TaskReport.cs
@@ -9,7 +9,7 @@ public class TaskReport : Auditable
     public string ReportContent { get; set; } = "No Content";
 
     public TaskActivity? TaskActivity { get; set; }
-    public int? TaskActivityId { get; set; }
+    public int TaskActivityId { get; set; }
 
     public IEnumerable<Evidence> Evidences { get; set; } = new List<Evidence>();
 }
---
+++ FTask.Repository/Migrations/20231015023313_Update a constraint for a field of TaskReport to be not null.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FTask.Repository.Migrations
+{
+    public partial class UpdateaconstraintforafieldofTaskReporttobenotnull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskReport_TaskActivity_TaskActivityId",
+                table: "TaskReport");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskReport_TaskActivityId",
+                table: "TaskReport");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskActivityId",
+                table: "TaskReport",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskReport_TaskActivityId",
+                table: "TaskReport",
+                column: "TaskActivityId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskReport_TaskActivity_TaskActivityId",
+                table: "TaskReport",
+                column: "TaskActivityId",
+                principalTable: "TaskActivity",
+                principalColumn: "TaskActivityId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskReport_TaskActivity_TaskActivityId",
+                table: "TaskReport");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskReport_TaskActivityId",
+                table: "TaskReport");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskActivityId",
+                table: "TaskReport",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskReport_TaskActivityId",
+                table: "TaskReport",
+                column: "TaskActivityId",
+                unique: true,
+                filter: "[TaskActivityId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskReport_TaskActivity_TaskActivityId",
+                table: "TaskReport",
+                column: "TaskActivityId",
+                principalTable: "TaskActivity",
+                principalColumn: "TaskActivityId");
+        }
+    }
+}
