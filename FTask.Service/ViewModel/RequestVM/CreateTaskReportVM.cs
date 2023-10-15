fbd719c2e1eeea676dc5d2d177867887f98fa9ed
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-15 12:26:50 +0700

    feat(vm): vm for task lecturer, activity and report

---
+++ FTask.Service/ViewModel/RequestVM/CreateTaskReportVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTask.Service.ViewModel.RequestVM.CreateTaskReport
+{
+    public class TaskReportVM
+    {
+        public string? ReportContent { get; set; }
+        [Required]
+        public int TaskActivityId { get; set; }
+        public IEnumerable<IFormFile> Evidences { get; set; } = new List<IFormFile>();
+    }
+}
