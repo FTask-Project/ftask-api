cec769a2a8d052964bf4297a743716e2d628263b
khoaLe12 <112998676+khoaLe12@users.noreply.github.com>
2023-11-11 12:28:48 +0700

    test cd

---
+++ FTask.API/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FTask.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+    }
+}
